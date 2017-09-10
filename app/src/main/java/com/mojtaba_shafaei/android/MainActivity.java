449c8f3a2b118efe1858bd9bf4345510e90cb1e1
mojtaba <shafaei.saeid@gmail.com>
2017-09-10 14:30:42 +0430

    add init with type

--- app/src/main/java/com/mojtaba_shafaei/android/MainActivity.java
+++ app/src/main/java/com/mojtaba_shafaei/android/MainActivity.java
@@ -2,6 +2,7 @@ package com.mojtaba_shafaei.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -13,7 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        em = findViewById(R.id.errorMessage);
+        em = findViewById(R.id.errorMessage2);
+        em.initWithType(ErrorMessage.NO_INTERNET);
         em.setOnClickListenerForButton(new View.OnClickListener() {
             @Override
             public void onClick(View view2) {
@@ -35,5 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 em.setVisibility(em.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
             }
         });
+
+        em.setIconTint(ContextCompat.getColorStateList(this,R.color.icon_color));
     }
 }
