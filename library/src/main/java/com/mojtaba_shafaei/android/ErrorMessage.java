aae4e2619853ba9797c50ade8de0b3ccd505218e
mojtaba <shafaei.saeid@gmail.com>
2017-12-31 16:10:35 +0330

    Add setButtonText(...)

--- library/src/main/java/com/mojtaba_shafaei/android/ErrorMessage.java
+++ library/src/main/java/com/mojtaba_shafaei/android/ErrorMessage.java
@@ -21,191 +21,199 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-
 import com.mojtaba_shafaei.androidErrorMessage.R;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 public class ErrorMessage extends LinearLayout {
-    private final String TAG = "ErrorMessageLibrary";
-    private ImageView icon;
-    private TextView message;
-    private Button button;
 
-    private OnClickListener clickListener;
+  private final String TAG = "ErrorMessageLibrary";
+  private ImageView icon;
+  private TextView message;
+  private Button button;
 
-    public static final int ERROR = 1;
-    public static final int MESSAGE = 2;
-    public static final int NO_INTERNET = 3;
-    public static final int NO_DATA = 4;
+  private OnClickListener clickListener;
 
-    @Retention(RetentionPolicy.SOURCE)
-    @Documented
-    @IntDef({ERROR, MESSAGE, NO_INTERNET, NO_DATA})
-    public @interface ErrorMessageTypeEnum {
+  public static final int ERROR = 1;
+  public static final int MESSAGE = 2;
+  public static final int NO_INTERNET = 3;
+  public static final int NO_DATA = 4;
+
+  @Retention(RetentionPolicy.SOURCE)
+  @Documented
+  @IntDef({ERROR, MESSAGE, NO_INTERNET, NO_DATA})
+  public @interface ErrorMessageTypeEnum {
+
+  }
+
+  public ErrorMessage(Context context) {
+    super(context);
+    init(context);
+  }
+
+  public ErrorMessage(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init(context);
+    readAttrs(context, attrs);
+  }
+
+  public ErrorMessage(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init(context);
+    readAttrs(context, attrs);
+  }
+
+  private void init(Context context) {
+    if (isInEditMode()) {
+      return;
     }
 
-    public ErrorMessage(Context context) {
-        super(context);
-        init(context);
-    }
+    View root = inflate(context, R.layout.message_error_layout, this);
+    root.setClickable(true);
 
-    public ErrorMessage(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(context);
-        readAttrs(context, attrs);
-    }
+    root.setOnTouchListener(new OnTouchListener() {
+      @Override
+      public boolean onTouch(View v, MotionEvent event) {
+        return true;
+      }
+    });
 
-    public ErrorMessage(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(context);
-        readAttrs(context, attrs);
-    }
+    icon = findViewById(R.id.imageViewIcon);
+    message = findViewById(R.id.textViewMessage);
+    button = findViewById(R.id.btn);
 
-    private void init(Context context) {
-        if (isInEditMode()) {
-            return;
+    button.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if (clickListener != null) {
+          clickListener.onClick(view);
         }
+      }
+    });
+  }
 
-        View root = inflate(context, R.layout.message_error_layout, this);
-        root.setClickable(true);
+  public void setMessageTypeface(Typeface typeface) {
+    message.setTypeface(typeface);
+    button.setTypeface(typeface);
+  }
 
-        root.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });
+  private void readAttrs(Context context, AttributeSet attrs) {
+    TypedArray a = context.getTheme().obtainStyledAttributes(
+        attrs,
+        R.styleable.ErrorMessage,
+        0, 0);
 
-        icon = findViewById(R.id.imageViewIcon);
-        message = findViewById(R.id.textViewMessage);
-        button = findViewById(R.id.btn);
+    try {
+      @ErrorMessageTypeEnum int typ = a.getInteger(R.styleable.ErrorMessage_em_type, ERROR);
+      initWithType(typ);
 
-        button.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (clickListener != null) {
-                    clickListener.onClick(view);
-                }
-            }
-        });
+      if (a.hasValue(R.styleable.ErrorMessage_em_icon)) {
+        Drawable iconRes = a.getDrawable(R.styleable.ErrorMessage_em_icon);
+        icon.setImageDrawable(iconRes);
+      }
+
+      if (a.hasValue(R.styleable.ErrorMessage_em_iconTintColor)) {
+        ColorStateList iconColorTint = a
+            .getColorStateList(R.styleable.ErrorMessage_em_iconTintColor);
+        setIconTint(iconColorTint);
+      }
+
+      if (a.hasValue(R.styleable.ErrorMessage_em_message)) {
+        String messageString = a.getString(R.styleable.ErrorMessage_em_message);
+        setMessage(messageString);
+      }
+
+      @ColorInt int colorInt = a.getColor(R.styleable.ErrorMessage_em_messageTextColor
+          , getResources().getColor(R.color.messageTextColor));
+
+      setMessageTextColor(colorInt);
+
+      if (a.hasValue(R.styleable.ErrorMessage_em_messageTextSize)) {
+        final int textSize = a.getDimensionPixelSize(R.styleable.ErrorMessage_em_messageTextSize,
+            getResources().getDimensionPixelSize(R.dimen.textSize));
+        message.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
+      }
+
+      setButtonVisibility(a.getInt(R.styleable.ErrorMessage_em_buttonVisibility, View.GONE));
+      setIconVisibility(a.getInt(R.styleable.ErrorMessage_em_iconVisibility, View.VISIBLE));
+
+      setIconSize(a.getDimensionPixelSize(R.styleable.ErrorMessage_em_iconSize,
+          getResources().getDimensionPixelSize(R.dimen.iconSize)));
+
+    } catch (Exception e) {
+      Log.e(TAG, "readAttrs: " + e.getMessage());
+    } finally {
+      a.recycle();
     }
+  }
 
-    public void setMessageTypeface(Typeface typeface) {
-        message.setTypeface(typeface);
-        button.setTypeface(typeface);
+  public void initWithType(@ErrorMessageTypeEnum int typ) {
+    switch (typ) {
+      case ERROR:
+        icon.setImageResource(R.drawable.ic_error);
+        break;
+
+      case MESSAGE:
+        icon.setImageResource(R.drawable.ic_done);
+        break;
+
+      case NO_INTERNET:
+        icon.setImageResource(R.drawable.ic_internet_off);
+        message.setText(R.string.no_internet_connection);
+        break;
+
+      case NO_DATA:
+        icon.setImageResource(R.drawable.ic_sad);
+        message.setText("no data found");
+        break;
     }
+  }
 
-    private void readAttrs(Context context, AttributeSet attrs) {
-        TypedArray a = context.getTheme().obtainStyledAttributes(
-                attrs,
-                R.styleable.ErrorMessage,
-                0, 0);
+  public void setIconVisibility(int visibility) {
+    icon.setVisibility(visibility);
+  }
 
-        try {
-            @ErrorMessageTypeEnum int typ = a.getInteger(R.styleable.ErrorMessage_em_type, ERROR);
-            initWithType(typ);
+  public void setIconSize(int dimensionPixelSize) {
+    LayoutParams layoutParams = (LayoutParams) icon.getLayoutParams();
+    layoutParams.width = dimensionPixelSize;
+    layoutParams.height = dimensionPixelSize;
+    icon.setLayoutParams(layoutParams);
+  }
 
-            if (a.hasValue(R.styleable.ErrorMessage_em_icon)) {
-                Drawable iconRes = a.getDrawable(R.styleable.ErrorMessage_em_icon);
-                icon.setImageDrawable(iconRes);
-            }
+  public void setMessage(String messageString) {
+    message.setText(messageString);
+  }
 
-            if (a.hasValue(R.styleable.ErrorMessage_em_iconTintColor)) {
-                ColorStateList iconColorTint = a.getColorStateList(R.styleable.ErrorMessage_em_iconTintColor);
-                setIconTint(iconColorTint);
-            }
+  public void setMessage(@StringRes int messageRes) {
+    message.setText(messageRes);
+  }
 
-            if (a.hasValue(R.styleable.ErrorMessage_em_message)) {
-                String messageString = a.getString(R.styleable.ErrorMessage_em_message);
-                setMessage(messageString);
-            }
+  public void setMessageTextColor(@ColorInt int colorInt) {
+    message.setTextColor(colorInt);
+  }
 
-            @ColorInt int colorInt = a.getColor(R.styleable.ErrorMessage_em_messageTextColor
-                    , getResources().getColor(R.color.messageTextColor));
+  public void setMessageTextColorRes(@ColorRes int colorRes) {
+    message.setTextColor(ContextCompat.getColor(getContext(), colorRes));
+  }
 
-            setMessageTextColor(colorInt);
-
-            if (a.hasValue(R.styleable.ErrorMessage_em_messageTextSize)) {
-                final int textSize = a.getDimensionPixelSize(R.styleable.ErrorMessage_em_messageTextSize, getResources().getDimensionPixelSize(R.dimen.textSize));
-                message.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize);
-            }
-
-            setButtonVisibility(a.getInt(R.styleable.ErrorMessage_em_buttonVisibility, View.GONE));
-            setIconVisibility(a.getInt(R.styleable.ErrorMessage_em_iconVisibility, View.VISIBLE));
-
-            setIconSize(a.getDimensionPixelSize(R.styleable.ErrorMessage_em_iconSize, getResources().getDimensionPixelSize(R.dimen.iconSize)));
-
-        } catch (Exception e) {
-            Log.e(TAG, "readAttrs: " + e.getMessage());
-        } finally {
-            a.recycle();
-        }
+  public void setButtonVisibility(int visibility) {
+    button.setVisibility(visibility);
+    if (visibility == View.VISIBLE) {
+      ViewCompat.setElevation(button,
+          getContext().getResources().getDimensionPixelSize(R.dimen.shadowSize));
     }
+  }
 
-    public void initWithType(@ErrorMessageTypeEnum int typ) {
-        switch (typ) {
-            case ERROR:
-                icon.setImageResource(R.drawable.ic_error);
-                break;
+  public void setOnClickListenerForButton(OnClickListener l) {
+    clickListener = l;
+  }
 
-            case MESSAGE:
-                icon.setImageResource(R.drawable.ic_done);
-                break;
+  public void setIconTint(ColorStateList iconColorTint) {
+    ImageViewCompat.setImageTintList(icon, iconColorTint);
+  }
 
-            case NO_INTERNET:
-                icon.setImageResource(R.drawable.ic_internet_off);
-                message.setText(R.string.no_internet_connection);
-                break;
-
-            case NO_DATA:
-                icon.setImageResource(R.drawable.ic_sad);
-                message.setText("no data found");
-                break;
-        }
-    }
-
-    public void setIconVisibility(int visibility) {
-        icon.setVisibility(visibility);
-    }
-
-    public void setIconSize(int dimensionPixelSize) {
-        LayoutParams layoutParams = (LayoutParams) icon.getLayoutParams();
-        layoutParams.width = dimensionPixelSize;
-        layoutParams.height = dimensionPixelSize;
-        icon.setLayoutParams(layoutParams);
-    }
-
-    public void setMessage(String messageString) {
-        message.setText(messageString);
-    }
-
-    public void setMessage(@StringRes int messageRes) {
-        message.setText(messageRes);
-    }
-
-    public void setMessageTextColor(@ColorInt int colorInt) {
-        message.setTextColor(colorInt);
-    }
-
-    public void setMessageTextColorRes(@ColorRes int colorRes) {
-        message.setTextColor(ContextCompat.getColor(getContext(), colorRes));
-    }
-
-    public void setButtonVisibility(int visibility) {
-        button.setVisibility(visibility);
-        if (visibility == View.VISIBLE) {
-            ViewCompat.setElevation(button, getContext().getResources().getDimensionPixelSize(R.dimen.shadowSize));
-        }
-    }
-
-    public void setOnClickListenerForButton(OnClickListener l) {
-        clickListener = l;
-    }
-
-    public void setIconTint(ColorStateList iconColorTint) {
-        ImageViewCompat.setImageTintList(icon, iconColorTint);
-    }
+  public void setButtonText(String buttonText) {
+    button.setText(buttonText);
+  }
 }
