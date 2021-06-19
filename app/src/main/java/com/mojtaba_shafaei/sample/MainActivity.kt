f746b571deefec6273f85767b7f271a2bf5dcefa
Mojtaba Shafaei <mjtb.shafaei@gmail.com>
2021-06-20 03:54:18 +0430

    1.2.10

--- app/src/main/java/com/mojtaba_shafaei/sample/MainActivity.kt
+++ app/src/main/java/com/mojtaba_shafaei/sample/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.mojtaba_shafaei.android.error_message.ErrorMessage.State
-import com.mojtaba_shafaei.sample.R
 import com.mojtaba_shafaei.sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
